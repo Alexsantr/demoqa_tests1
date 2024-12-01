59f5a16f35e60956469c381d5407cd61a10a5978
Trek119 <kopen_kin@mail.ru>
2024-12-01 20:53:11 +0300

    add new class DragAndDropTests and SearchIntoGithubTests

---
+++ src/test/java/tests/DragAndDropTests.java
@@ -0,0 +1,40 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl="https://the-internet.herokuapp.com/drag_and_drop";
+    }
+
+    @Test
+    void actionsTest() {
+        open("");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(150, 1).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+    @Test
+    void dragAndDropTest(){
+        open("");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+}
---
+++ src/test/java/tests/SearchIntoGithubTests.java
@@ -0,0 +1,33 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class SearchIntoGithubTests {
+
+        @BeforeAll
+        static void beforeAll() {
+            Configuration.browserSize = "1920x1080";
+            Configuration.baseUrl = "https://github.com";
+            Configuration.pageLoadStrategy = "eager";
+            Configuration.holdBrowserOpen=true;
+        }
+        @Test
+    void searchIntoGithubTest(){
+            open("");
+            $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+            $(byTagAndText("a","Enterprises")).click();
+            $(".enterprise-hero-grid").shouldHave(text("The AI-powered developer platform"));
+
+        }
+
+}
+
+
